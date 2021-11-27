29cea82b1f4a52fee613012d5c20b66446693597
Cerber Ursi <dev+rust@cerbe.ru>
2021-11-27 21:04:42 +0700

    rustfmt + clippy

--- src/lib.rs
+++ src/lib.rs
@@ -3,94 +3,101 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as pm2_ts;
 
-use syn::{parse2, ItemTrait, Ident, TraitItem::*, spanned::Spanned, ReturnType, FnArg};
 use quote::quote;
+use syn::{parse2, spanned::Spanned, FnArg, Ident, ItemTrait, ReturnType, TraitItem::*};
 
 #[proc_macro_attribute]
 pub fn object_safe(_attr: TokenStream, item: TokenStream) -> TokenStream {
-  let orig_trait: ItemTrait = match parse2(pm2_ts::from(item)) {
-    Ok(item) => item,
-    Err(err) => return err.to_compile_error().into(),
-  };
+    let orig_trait: ItemTrait = match parse2(pm2_ts::from(item)) {
+        Ok(item) => item,
+        Err(err) => return err.to_compile_error().into(),
+    };
 
-  if let Some(where_clause) = orig_trait.generics.where_clause.clone() {
-    if check_self_sized(&where_clause) {
-      return quote::quote_spanned! {
-        where_clause.span() =>
-        compile_error!("Trait with Self: Sized trait bound cannot be made object-safe");
-      }.into();
+    if let Some(where_clause) = orig_trait.generics.where_clause.clone() {
+        if check_self_sized(&where_clause) {
+            return quote::quote_spanned! {
+              where_clause.span() =>
+              compile_error!("Trait with Self: Sized trait bound cannot be made object-safe");
+            }
+            .into();
+        }
     }
-  }
 
-  let mut new_trait = orig_trait.clone();
-  let orig_trait_name = orig_trait.ident.clone();
+    let mut new_trait = orig_trait.clone();
+    let orig_trait_name = orig_trait.ident.clone();
 
-  let new_trait_name = Ident::new(&format!("ObjectSafe{}", orig_trait_name), orig_trait_name.span());
-  new_trait.ident = new_trait_name.clone();
-  new_trait.items.retain(|item| match item {
-    Const(_) => false,
-    Method(item) => check_obj_safe(item),
-    Type(_) => false,
-    Macro(_) => false,
-    Verbatim(_) => false,
-    _ => false,
-  });
+    let new_trait_name = Ident::new(
+        &format!("ObjectSafe{}", orig_trait_name),
+        orig_trait_name.span(),
+    );
+    new_trait.ident = new_trait_name.clone();
+    new_trait.items.retain(|item| match item {
+        Const(_) => false,
+        Method(item) => check_obj_safe(item),
+        Type(_) => false,
+        Macro(_) => false,
+        Verbatim(_) => false,
+        _ => false,
+    });
 
-  let impl_items = new_trait.items.iter().cloned().map(|item| impl_trait_item(orig_trait_name.clone(), item));
+    let impl_items = new_trait
+        .items
+        .iter()
+        .cloned()
+        .map(|item| impl_trait_item(orig_trait_name.clone(), item));
 
-  let output = quote! {
-    #orig_trait
-    #new_trait
+    let output = quote! {
+      #orig_trait
+      #new_trait
 
-    impl<T: #orig_trait_name> #new_trait_name for T {
-      #(#impl_items)*
-    }
-  };
-  output.into()
+      impl<T: #orig_trait_name> #new_trait_name for T {
+        #(#impl_items)*
+      }
+    };
+    output.into()
 }
 
 fn check_self_sized(item: &syn::WhereClause) -> bool {
-  unimplemented!();
+    unimplemented!();
 }
 
 fn check_obj_safe(item: &syn::TraitItemMethod) -> bool {
-  let sig = &item.sig;
-  if let Some(where_clause) = &sig.generics.where_clause {
-    if check_self_sized(where_clause) {
-      return true;
+    let sig = &item.sig;
+    if let Some(where_clause) = &sig.generics.where_clause {
+        if check_self_sized(where_clause) {
+            return true;
+        }
     }
-  }
-  if let Some(FnArg::Typed(_)) = sig.inputs.first() {
-    false
-  } else if let ReturnType::Type(_, return_type) = &sig.output {
-    unimplemented!()
-  } else {
-    true
-  }
+    if let Some(FnArg::Typed(_)) = sig.inputs.first() {
+        false
+    } else if let ReturnType::Type(_, return_type) = &sig.output {
+        unimplemented!()
+    } else {
+        true
+    }
 }
 
 fn impl_trait_item(orig_ident: Ident, trait_item: syn::TraitItem) -> syn::ImplItemMethod {
-  let span = trait_item.span().clone();
-  match trait_item {
-    Method(item) => {
-      let syn::TraitItemMethod { attrs, sig, .. } = item;
-      let name = sig.ident.clone();
-      let inputs = sig.inputs.clone().into_iter().map(|item| {
-        match item {
-          FnArg::Receiver(_) => quote!{ self },
-          FnArg::Typed(pat) => {
-            let pat = pat.pat;
-            quote!{ #pat }
-          }
+    let span = trait_item.span();
+    match trait_item {
+        Method(item) => {
+            let syn::TraitItemMethod { attrs, sig, .. } = item;
+            let name = sig.ident.clone();
+            let inputs = sig.inputs.clone().into_iter().map(|item| match item {
+                FnArg::Receiver(_) => quote! { self },
+                FnArg::Typed(pat) => {
+                    let pat = pat.pat;
+                    quote! { #pat }
+                }
+            });
+            parse2(quote::quote_spanned! {
+              span =>
+              #(#attrs)* #sig {
+                <Self as #orig_ident>::#name(#(#inputs)*)
+              }
+            })
+            .expect("Internal error, macro generated wrong code for method impl")
         }
-      });
-      parse2(quote::quote_spanned! {
-        span =>
-        #(#attrs)* #sig {
-          <Self as #orig_ident>::#name(#(#inputs)*)
-        }
-      }).expect("Internal error, macro generated wrong code for method impl")
-    },
-    _ => unreachable!(),
-  }
+        _ => unreachable!(),
+    }
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,23 +1,23 @@
 mod traits {
 
-  #[object_safe::object_safe]
-  pub trait Test {
-    fn test(&self);
-  }
-
-  pub struct TestStruct;
-
-  impl Test for TestStruct {
-    fn test(&self) {
-      println!("It did not crash!");
+    #[object_safe::object_safe]
+    pub trait Test {
+        fn test(&self);
     }
-  }
+
+    pub struct TestStruct;
+
+    impl Test for TestStruct {
+        fn test(&self) {
+            println!("It did not crash!");
+        }
+    }
 }
 
 use traits::{ObjectSafeTest, TestStruct};
 
 #[test]
 fn works() {
-  let test = TestStruct;
-  test.test();
+    let test = TestStruct;
+    test.test();
 }
